30253945b1358666e3401727819b7915a4f3bdf7
northpanupong <panupong.w@wewebplus.com>
2023-02-09 00:53:07 +0700

    vercel

--- routes/auth_service.routes.js
+++ routes/auth_service.routes.js
@@ -2,5 +2,5 @@ const general = require('../middlewares/general.middleware');
 
 module.exports = function(app) {
     var auth = require('../controllers/auth_service.controller');
-    app.post('/dev22-sei/service-api/auth', auth.auth);
+    app.post('/service-api/auth', auth.auth);
 }
--- routes/services/api_mobile/api_mobile.routes.js
+++ routes/services/api_mobile/api_mobile.routes.js
@@ -5,7 +5,7 @@ var serviceName = "api_mobile";
 var api_mobile = require('../../../controllers/api_mobile.controller');
 
 module.exports = function (app) {
-    app.all('/dev22-sei/service-api/home/' + serviceName, general.verifyToken, api_mobile.init, function (req, res, next) {
+    app.all('/service-api/home/' + serviceName, general.verifyToken, api_mobile.init, function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", ["X-Requested-With",'Content-Type']);
         next();
