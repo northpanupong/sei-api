1dc169f1f07c23a36f2d4f7ebd8b5d867522c37d
northpanupong <panupong.w@wewebplus.com>
2023-02-20 17:15:58 +0700

    Update sei_db.js

--- config/databases/sei_db.js
+++ config/databases/sei_db.js
@@ -3,7 +3,7 @@ var util = require('util');
 
 exports.connectDB = function () {
     return mysql.createConnection({
-        host: "192.168.1.129",
+        host: "183.88.224.221",
         user: "root",
         password: "IySY?Pk7!!mH",
         database: "2022_sei",
